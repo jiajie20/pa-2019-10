cac78cff3c78eaf7bbf6fc537b603f2c4db6e63d
Anny He <anny.he@salesforce.com>
2019-10-05 18:47:34 -0700

    works

--- index.js
+++ index.js
@@ -5,19 +5,28 @@ const path = require('path')
 
 app.use(express.static('public')); // for serving the HTML file
 
-const upload = multer({ dest: __dirname + '/public/uploads/' });
-const type = upload.single('upl');
-
-app.use('/public', express.static('public'));
-
 app.get('/', function(req, res) {
    res.sendFile(path.join(__dirname + '/client/public/index.html'))
-})
+}) 
 
-app.post('/api/test', type, function (req, res) {
+const storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+     cb(null, 'uploads/')
+   },
+   filename: function (req, file, cb) {
+     cb(null, Date.now() + '.ogg') //Appending .jpg
+   }
+ })
+ 
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('upl'), function (req, res) {
    console.log(req.body);
    console.log(req.file);
+   res.send('ok')
    // do stuff with file
 });
 
-app.listen(3000);
+app.listen(8080);
+
+process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); })
